9f000098dcf697d2785ca6a310434dca21e1dfe3
Harshal Wandhare <114572157+harshalbw7@users.noreply.github.com>
2024-01-25 19:16:44 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 buildscript {
     dependencies {
-        classpath("com.google.gms:google-services:4.4.0")
+        classpath("com.google.gms:google-services:2.4.4")
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.1" apply false
+    id("com.android.application") version "4.2.1" apply false
 }
